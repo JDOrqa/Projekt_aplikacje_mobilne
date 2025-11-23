bfb3f7d0c4b9c75de31402fda4a4f9a0a4fa9cf6
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 16:04:42 +0100

    poczatek

--- app/src/main/java/com/example/slotmaster/GameHistoryManager.kt
+++ app/src/main/java/com/example/slotmaster/GameHistoryManager.kt
@@ -5,3 +5,9 @@ import android.database.sqlite.SQLiteDatabase
 import com.example.slotmaster.models.GameHistory
 import java.text.SimpleDateFormat
 import java.util.*
+class GameHistoryManager(private val dbHelper: GameHistoryHelper) {
+
+
+    companion object {
+        private const val TABLE_HISTORY = "game_history"
+    }
