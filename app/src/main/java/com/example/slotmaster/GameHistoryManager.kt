c5253eea5fc5a681992ad4edce8f6f499a94eac7
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 16:04:14 +0100

    Stowrzenie

---
+++ app/src/main/java/com/example/slotmaster/GameHistoryManager.kt
@@ -0,0 +1,7 @@
+package com.example.slotmaster
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import com.example.slotmaster.models.GameHistory
+import java.text.SimpleDateFormat
+import java.util.*
