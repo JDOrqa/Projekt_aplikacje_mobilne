f3f2ec9bb6cd28ada56867c2f2801cae2d257969
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-11-30 16:28:14 +0100

    Add import for withContext in FirebirdApiManager

--- app/src/main/java/com/example/slotmaster/models/FirebirdApiManager.kt
+++ app/src/main/java/com/example/slotmaster/models/FirebirdApiManager.kt
@@ -3,3 +3,4 @@ import android.content.Context
 import android.util.Log
 import com.example.slotmaster.models.GameHistory
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
