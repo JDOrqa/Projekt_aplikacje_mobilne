6e92a21152a144c1804cd407f1e4fab6658c1da6
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-30 22:01:23 +0100

    Add necessary imports for MapActivity

--- app/src/main/java/com/example/slotmaster/MapActivity.kt
+++ app/src/main/java/com/example/slotmaster/MapActivity.kt
@@ -1,2 +1,20 @@
 package com.example.slotmaster
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.location.Location
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Polygon
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import android.util.Log
