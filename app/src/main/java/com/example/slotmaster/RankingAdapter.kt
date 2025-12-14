4ef5a6ee4b071a10e07d10452574b628c747657f
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-12-14 13:24:50 +0100

    Update RankingAdapter.kt

--- app/src/main/java/com/example/slotmaster/RankingAdapter.kt
+++ app/src/main/java/com/example/slotmaster/RankingAdapter.kt
@@ -6,3 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+class RankingAdapter(private var rankingItems: List<RankingItem>) : 
+    RecyclerView.Adapter<RankingAdapter.RankingViewHolder>() {
+
+    fun updateData(newItems: List<RankingItem>) {
+        // Przypisz poprawne miejsca w rankingu
+        val rankedItems = newItems.mapIndexed { index, item ->
+            item.copy(rank = index + 1)
+        }
+        this.rankingItems = rankedItems
+        notifyDataSetChanged()
+    }
